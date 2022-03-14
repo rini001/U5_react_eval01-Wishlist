da22e7eb19784022bdac6798d88097d6a0f15239
rini960 <debnathrenaissance95@gmail.com>
2022-03-14 22:11:58 +0530

    very little changes

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
-import { Vegetables } from "./Vegetables";
 import styles from "./Wishlist.module.css"
 export const Wishlist = () => {
   const [query, setQuery] = useState("");
